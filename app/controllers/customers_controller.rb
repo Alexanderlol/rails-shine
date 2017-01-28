98ecbfac6ca847435c42b89c05f2d917b6c852e4
Alex Afeltra <afeltra.alex@gmail.com>
2017-01-28 15:11:49 +0000

    added angular 1.5

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -7,7 +7,7 @@ class CustomersController < ApplicationController
       customer_search_term = CustomerSearchTerm.new(@keywords)
       @customers = Customer.where(
         customer_search_term.where_clause,
-        customer_search_term.where_args).order(customer_search_term.order)
+        customer_search_term.where_args).order(customer_search_term.order).
         offset(PAGE_SIZE * @page).limit(PAGE_SIZE)
     else
       @customers = []
